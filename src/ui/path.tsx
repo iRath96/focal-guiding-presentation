d47145b46d7d59148717a9e28fb23f0445e9e4b2
Alexander Rath <alex@ist.besonders.cool>
2023-07-17 13:35:03 +0200

    minor improvements

--- src/ui/path.tsx
+++ src/ui/path.tsx
@@ -1,4 +1,4 @@
-import { Line, View2D, Node } from '@motion-canvas/2d'
+import { Line, View2D, Node, Layout } from '@motion-canvas/2d'
 import { Vector2f } from '../rt/math'
 
 export enum PathVertexType {
@@ -15,6 +15,7 @@ export interface PathVertex {
 }
 
 interface PVPath {
+    root: Node
     segments: Node[]
 }
 
@@ -27,12 +28,14 @@ export class PathVisualizer {
     ) {}
 
     showPath(path: PathVertex[]) {
-        const pvp: PVPath = { segments: [] }
+        const root = <Layout />
+        this.view.add(root)
+
+        const pvp: PVPath = { root, segments: [] }
         for (let i = 1; i < path.length; i++) {
             const segment = <Line
                 points={[ path[i-1].p, path[i].p ]}
                 stroke="#ffffff"
-                opacity={0.5}
                 lineWidth={4}
                 endArrow={
                     path[i].type == PathVertexType.Diffuse
@@ -40,7 +43,7 @@ export class PathVisualizer {
                 zIndex={2}
             />
             pvp.segments.push(segment)
-            this.view.add(segment)
+            root.add(segment)
         }
 
         const id = this.nextId++
@@ -48,11 +51,13 @@ export class PathVisualizer {
         return id
     }
 
+    getPath(id: number) {
+        return this.shownPaths.get(id).root
+    }
+
     removePath(id: number) {
         if (!this.shownPaths.has(id)) return
-        for (const segment of this.shownPaths.get(id).segments) {
-            segment.remove()
-        }
+        this.shownPaths.get(id).root.remove()
         this.shownPaths.delete(id)
     }
 }
