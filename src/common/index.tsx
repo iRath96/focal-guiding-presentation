a131c2ad6dab89c658fdaf59cb9a6f1ac227f749
Alexander Rath <alex@ist.besonders.cool>
2023-07-26 09:34:54 +0200

    add colors, for use later

---
+++ src/common/index.tsx
@@ -0,0 +1,8 @@
+export const colors = {
+    background: "#262626",
+    red:        "#C92829",
+    yellow:     "#FDA400",
+    green:      "#95C627",
+    olive:      "#09A790",
+    blue:       "#06B2DF",
+}
